560021d59cd636d80f142e6959b0b82056fc83f0
Mike Chau <mchau@iwu.edu>
2013-02-28 20:59:32 -0600

    add some codecademy examples

---
+++ case_example.rb
@@ -0,0 +1,14 @@
+# case statement
+
+var = "hello"
+
+case var
+when "hello"
+  puts "var is hello!"
+when "hi"
+  puts "var is hi!"
+when "yo"
+  puts "var is yo!"
+else
+  puts "I dunno..."
+end
---
+++ conditionals.rb
@@ -0,0 +1,9 @@
+# conditions
+
+# one line if/unless
+
+puts "hello world" if false
+puts "hello world" if true
+
+puts "hello world" unless false # equiv to if true
+puts "hello world" unless true # equiv to if false
