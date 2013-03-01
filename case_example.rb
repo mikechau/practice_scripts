394b69d1c7b1b8c74b588191683e49ac79dd37db
Mike Chau <mchau@iwu.edu>
2013-03-01 07:11:55 -0600

    Update conditions and case examples

--- case_example.rb
+++ case_example.rb
@@ -12,3 +12,12 @@ when "yo"
 else
   puts "I dunno..."
 end
+
+# case statement, shorter
+
+case var
+  when "hello" then puts "var is hello!"
+  when "hi" then puts "var is hi!"
+  when "yo" then puts "var is yo!"
+  else puts "I don't know!"
+end
--- conditionals.rb
+++ conditionals.rb
@@ -1,9 +1,14 @@
 # conditions
 
 # one line if/unless
-
 puts "hello world" if false
 puts "hello world" if true
 
 puts "hello world" unless false # equiv to if true
 puts "hello world" unless true # equiv to if false
+
+# ternary conditional expression
+# boolean ? Do this if true: Do this if false
+
+puts 3 < 4 ? "3 is less than 4!" : "3 is not less than 4."
+puts 10 > 11 ? "10 is not less than 11 is true!" : "10 is not 11 is false!"
